51fed440115ccada56ec7883e42a0616dbb6f2fc
IT-Spezialist-des-Asozialen <89655752+IT-Spezialist-des-Asozialen@users.noreply.github.com>
2022-01-21 22:46:06 +0100

    Update Youtubedl.py

--- Youtubedl.py
+++ Youtubedl.py
@@ -2,17 +2,18 @@ import os
 path='./youtube-dl/youtube-dl '#this is the path where youtube-dl is safed
 i=0
 links=[]
-folder=""
-folders=[]
+#folder=""
+#folders=[]
 wait = "t"
 while wait!="y":
     singleLink=input("Please insert the link to the video: ")
-    folder=input("Where should the video be safed? Please insert the complet path. If you insert nothing, the default path is choosen.")
-    folders+=[folder]
+    #folder=input("Where should the video be safed? Please insert the complet path. If you insert nothing, the default path is choosen.")
+    #folders+=[folder]
     links += [singleLink]
     wait=input('If you want to safe all videos, tipe "y". If you want to add others, tipe "Enter".')
     i+=1
-print("All videos were safed in the respective folder.")
+#print("All videos were safed in the respective folder.")
+print("All videos were safed in the default folder.")
 numbersLinks=len(links)
 i=0
 while i <numbersLinks:
